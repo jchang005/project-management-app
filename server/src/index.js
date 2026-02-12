340a3cd8817af7bf3474908dfc5eb0c9e86b629d
jchang005 <joshua.chang998@gmail.com>
2026-02-11 22:41:44 -0500

    created authentication middleware

--- server/src/index.js
+++ server/src/index.js
@@ -14,9 +14,9 @@ const db = new pg.Pool({
   connectionString: DATABASE_URL,
 });
 
-db.query("SELECT 1")
-  .then(() => console.log("✅ Postgres connected"))
-  .catch((err) => console.error("❌ DB error", err));
+// db.query("SELECT 1")
+//   .then(() => console.log("✅ Postgres connected"))
+//   .catch((err) => console.error("❌ DB error", err));
 
 app.post("/register", async (req, res) => {
   const { email, password, firstName, lastName, phoneNumber } = req.body;
@@ -60,87 +60,59 @@ app.post("/login", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT password_hash FROM users WHERE email=$1",
+      "SELECT id, password_hash FROM users WHERE email=$1",
       [email],
     );
-    if (result.rows.length >= 1) {
-      var validPasssowrd = bcrypt.compare(
-        password,
-        result.rows[0].password_hash,
-      );
-      if (validPasssowrd) {
-        var token = jwt.sign(email, JWT_SECRET, { expiresIn: "15m" });
-        return res.status(200).json({
-          token: token,
-          message: "Login successful",
-        });
-      } else {
-        return res.status(400).json({
-          error: "INCORRECT_PASSWORD",
-          message: "Incorrect password",
-        });
-      }
-    } else {
+
+    if (result.rows.length === 0) {
       return res.status(400).json({
-        error: "USER_NOT_FOUND",
-        message: "User does not exist",
+        error: "INVALID_CREDENTIALS",
+        message: "Invalid email or password",
       });
     }
+
+    var validPasssowrd = bcrypt.compare(password, result.rows[0].password_hash);
+    if (!validPasssowrd) {
+      return res.status(400).json({
+        error: "INCORRECT_PASSWORD",
+        message: "Incorrect password",
+      });
+    }
+
+    var token = jwt.sign({ userId: result.rows[0].id, email }, JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    return res.status(200).json({
+      token: token,
+      message: "Login successful",
+    });
   } catch (err) {
     return res.status(400).json({
-      error: "USER_NOT_FOUND",
-      message: "User does not exist",
+      error: "SERVER_ERROR",
+      message: "Something went wrong",
     });
   }
 });
 
-app.post("/teams/:teamId/projects", async (req, res) => {
+app.use(authenticate);
+
+app.post("/teams/:teamId/projects", async (req, res) => {});
+
+function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return res.status(401).json({ error: "Not logged in" });
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Invalid token format" });
   }
 
-  var user;
-
+  const token = authHeader.split(" ")[1];
   try {
-    const token = authHeader.split(" ")[1];
-    user = jwt.verify(token, JWT_SECRET);
-  } catch {
-    return res.status(401).json({ error: "Invalid token" });
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid or expired token" });
   }
-
-  const teamId = req.params.teamId;
-
-  const membership = await db.query(
-    `
-    SELECT role
-    FROM team_members
-    WHERE team_id = $1 AND user_id = $2
-    `,
-    [teamId, user.userId],
-  );
-  if (membership.rows.length === 0) {
-    return res.status(403).json({ error: "Not a team member" });
-  }
-
-  const role = membership.rows[0].role;
-  if (role !== "OWNER" && role !== "MEMBER") {
-    return res.status(403).json({ error: "Not allowed" });
-  }
-
-  // finally, create the project
-  const project = await db.query(
-    `
-    INSERT INTO projects (team_id, name)
-    VALUES ($1, $2)
-    RETURNING *
-    `,
-    [teamId, req.body.name],
-  );
-
-  res.status(201).json(project.rows[0]);
-});
+}
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
