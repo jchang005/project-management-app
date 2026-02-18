63b4760c3d42804311a446e0f11a8d2a969b9c9c
jchang005 <joshua.chang998@gmail.com>
2026-02-17 22:27:06 -0500

    added creat task and get team endpoints

--- queries.sql
+++ queries.sql
@@ -1,24 +1,46 @@
 CREATE TABLE users (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
-  pword TEXT NOT NULL,
-  first_name TEXT,
-  last_name TEXT,
-  phone_number TEXT
+  pword TEXT NOT NULL
 );
 
 CREATE TABLE teams (
-  team_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  team_id SERIAL PRIMARY KEY,
   team_name TEXT UNIQUE NOT NULL,
-  team_owner TEXT,
-  FOREIGN KEY (team_owner) REFERENCES users(email)
+  team_owner INT,
+  FOREIGN KEY (team_owner) REFERENCES users(id) ON DELETE CASCADE
 )
 
 CREATE TABLE users_teams (
-  user_id INTEGER,
-  team_id INTEGER, 
+  user_id INT,
+  team_id INT, 
   team_role TEXT, 
   PRIMARY KEY (user_id, team_id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE
+)
+
+SELECT ut.team_role
+FROM users_teams ut
+JOIN teams t ON ut.team_id = t.team_id
+WHERE t.user_id = $1 AND ut.user_id = $1
+
+// Tasks are bucketed in a Team
+CREATE TABLE tasks (
+  task_id SERIAL PRIMARY KEY,
+  title TEXT,
+  content TEXT,
+  assigned_to INT,
+  team_id INT NOT NULL,
+  task_status TEXT NOT NULL DEFAULT 'To do'
+    CHECK (task_status IN ('To do', 'In progress', 'Complete'))
   FOREIGN KEY (team_id) REFERENCES teams(team_id)
+)
+
+CREATE TABLE tasks_teams (
+  task_id INT,
+  team_id INT,
+  FOREIGN KEY (task_id) REFERENCES tasks(task_id) ON DELETE CASCADE,
+  FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE,
+  PRIMARY KEY (task_id, team_id)
 )
--- server/src/index.js
+++ server/src/index.js
@@ -19,14 +19,15 @@ const db = new pg.Pool({
 //   .catch((err) => console.error("❌ DB error", err));
 
 app.post("/register", async (req, res) => {
-  const { email, password, firstName, lastName, phoneNumber } = req.body;
+  const { email, password } = req.body;
 
   // check if user already exists
   // Email is already registered to an account.
   try {
-    const result = await db.query("SELECT id From users WHERE email = $1", [
-      email,
-    ]);
+    const result = await db.query(
+      "SELECT user_id From users WHERE email = $1",
+      [email],
+    );
     console.log(result);
     if (result.rows.length >= 1) {
       // user exists
@@ -38,10 +39,10 @@ app.post("/register", async (req, res) => {
       // Hash password
       const hash = await bcrypt.hash(password, 10);
       try {
-        await db.query(
-          "INSERT INTO users (email, pword, first_name, last_name, phone_number) VALUES ($1, $2, $3, $4, $5)",
-          [email, hash, firstName, lastName, phoneNumber],
-        );
+        await db.query("INSERT INTO users (email, pword) VALUES ($1, $2)", [
+          email,
+          hash,
+        ]);
       } catch (err) {
         console.log(err);
         return res.status(500).json("Server Error: Could not register user.");
@@ -60,7 +61,7 @@ app.post("/login", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT id, pword FROM users WHERE email=$1",
+      "SELECT user_id, pword FROM users WHERE email=$1",
       [email],
     );
 
@@ -79,7 +80,7 @@ app.post("/login", async (req, res) => {
       });
     }
 
-    var token = jwt.sign({ userId: result.rows[0].id, email }, JWT_SECRET);
+    var token = jwt.sign({ userId: result.rows[0].user_id, email }, JWT_SECRET);
     return res.status(200).json({
       token: token,
       message: "Login successful",
@@ -99,43 +100,101 @@ app.post("/createteam", async (req, res) => {
   // insert into teams table
   // set user as owner of team
 
-  const { userId, email } = req.user;
+  const { userId } = req.user;
   const { teamName } = req.body;
 
   try {
-    const result = await db.query(
-      "SELECT team_id FROM teams WHERE team_name=$1",
-      [teamName],
+    // Create team
+    const insertTeam = await db.query(
+      "INSERT INTO teams (team_name, team_owner) values ($1, $2) RETURNING team_id",
+      [teamName, userId],
+    );
+    // Set "OWNER" role
+    await db.query(
+      "INSERT INTO users_teams (user_id, team_id, team_role) values ($1, $2, $3)",
+      [userId, insertTeam.rows[0].team_id, "OWNER"],
     );
 
-    if (result.rows.length === 0) {
-      // Create team
-      await db.query(
-        "INSERT INTO teams (team_name, team_owner) values ($1, $2)",
-        [teamName, email],
-      );
-      // Get teamID that was just generated
-      const teamId = await db.query(
-        "SELECT team_id FROM teams WHERE team_name = $1",
-        [teamName],
-      );
-      // Set "OWNER" role
-      await db.query(
-        "INSERT INTO users_teams (user_id, team_id, team_role) values ($1, $2, $3)",
-        [userId, teamId.rows[0].team_id, "OWNER"],
-      );
-
-      return res.status(200).json({ message: "Team successfully created!" });
-    } else {
-      return res.status(400).json({ message: "team already exists" });
-    }
+    return res.status(201).json({ message: "Team successfully created!" });
   } catch (err) {
+    if (err.code === "23505") {
+      return res.status(409).json({ message: "Team already exists" });
+    }
+    console.log(err);
     return res.status(500).json({
       error: err.messasge,
       message: "Something went wrong",
     });
   }
 });
+
+app.post("/team/:teamId/tasks", async (req, res) => {
+  const { userId } = req.user;
+  const { title, content, assignedTo } = req.body;
+  const { teamId } = req.params;
+
+  try {
+    const checkRole = await db.query(
+      `SELECT team_role 
+      FROM users_teams 
+      WHERE user_id=$1 
+      AND team_id=$2`,
+      [userId, teamId],
+    );
+
+    if (checkRole.rows.length === 0) {
+      return res.status(403).json({
+        error: "NOT_A_MEMBER",
+        message: "You are not part of this team",
+      });
+    }
+
+    const role = checkRole.rows[0].team_role;
+
+    if (!["MEMBER", "OWNER"].includes(role)) {
+      return res.status(403).json({
+        error: "INSUFFICIENT_ROLE",
+        message: "You do not have permission to create tasks",
+      });
+    }
+
+    await db.query(
+      `INSERT INTO tasks (title, content, assigned_to, team_id) 
+      VALUES ($1, $2, $3, $4)`,
+      [title, content, assignedTo, teamId],
+    );
+
+    return res.status(200).json({ message: "Task created" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      error: "SERVER_ERROR",
+      message: "Something went wrong",
+    });
+  }
+});
+
+// get team: click into team tile, returns team members, tasks,
+app.get("/team/:teamId", async (req, res) => {
+  const teamId = req.params.teamId;
+
+  // store team members into a list
+  // store tasks assigned to team
+  try {
+    const teamMembers = await db.query(
+      "SELECT (user_id, team_role) FROM users_teams WHERE team_id=$1 ",
+      [teamId],
+    );
+    const tasks = await db.query("SELECT* FROM tasks_teams WHERE team_id=$1", [
+      teamId,
+    ]);
+    // do stuff with them later
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("");
 
 function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
